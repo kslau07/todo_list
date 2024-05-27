39565139811bf10269ee5b5adf2bc7cfa96c565f
sam <abluecar@gmail.com>
2024-05-26 23:31:09 -0400

    feat: Use localStorage to save data
    Create function to check if localStorage is available
    Create function to stringify projects
    Create function to parse localStorage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./style.css";
+import { storageAvailable } from "./storageAvailable";
+
+const hasStorage = (type) => (storageAvailable(type) ? true : false);
+
+const projects = [];
+
+function createProject(projectName) {
+  return {
+    projectName: projectName,
+  };
+}
+
+function loadProjects() {
+  if (localStorage.getItem("projects")) {
+    const savedProjects = JSON.parse(localStorage.getItem("projects"));
+  } else {
+    console.log("No local save file found.");
+  }
+}
+
+function saveProjects() {
+  if (!hasStorage("localStorage")) return;
+
+  localStorage.setItem("projects", JSON.stringify(projects));
+}
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  const projectName = document.querySelector("input").value;
+  const newProject = createProject(projectName);
+  projects.push(newProject);
+  saveProjects();
+});
+
+loadProjects();
