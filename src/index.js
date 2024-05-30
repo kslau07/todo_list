d33cdb0f3fb496d713649808e6e7cfa8daf55b7e
sam <abluecar@gmail.com>
2024-05-30 00:59:20 -0400

    style: Slide-in modal

--- src/index.js
+++ src/index.js
@@ -1,36 +1,81 @@
 import "./style.css";
 import { storageAvailable } from "./storageAvailable";
 
-const hasStorage = (type) => (storageAvailable(type) ? true : false);
-
 const projects = [];
 
-function createProject(projectName) {
-  return {
-    projectName: projectName,
-  };
-}
+const hasStorage = (type) => (storageAvailable(type) ? true : false);
 
-function loadProjects() {
-  if (localStorage.getItem("projects")) {
-    const savedProjects = JSON.parse(localStorage.getItem("projects"));
-  } else {
-    console.log("No local save file found.");
-  }
-}
-
-function saveProjects() {
+const saveProjects = function () {
   if (!hasStorage("localStorage")) return;
 
   localStorage.setItem("projects", JSON.stringify(projects));
-}
+};
 
-const btn = document.querySelector("button");
-btn.addEventListener("click", () => {
-  const projectName = document.querySelector("input").value;
-  const newProject = createProject(projectName);
-  projects.push(newProject);
+const createProject = function (projectName) {
+  return {
+    projectName,
+  };
+};
+
+const initializeProjectsWithDefault = function () {
+  const defaultProject = createProject("default");
+  projects.push(defaultProject);
+};
+
+const loadProjects = function () {
+  if (localStorage.getItem("projects")) {
+    const savedProjects = JSON.parse(localStorage.getItem("projects"));
+    savedProjects.forEach((savedProject) => projects.push(savedProject));
+  } else {
+    initializeProjectsWithDefault();
+  }
+};
+
+const buttonCreateProject = document.querySelector(".button-create-project");
+buttonCreateProject.addEventListener("click", () => {
+  const projectNameInput = document.querySelector("input");
+  const newProjectName = projectNameInput.value;
+  const newProjectObject = createProject(newProjectName);
+  projects.push(newProjectObject);
   saveProjects();
+  refreshUI();
+  projectNameInput.value = "";
 });
 
+const populateProjects = function () {
+  const projectsUl = document.querySelector(".projects");
+  projectsUl.innerHTML = "";
+
+  projects.forEach((project) => {
+    const li = document.createElement("li");
+    li.textContent = project.projectName;
+    projectsUl.appendChild(li);
+  });
+};
+
+function refreshUI() {
+  populateProjects();
+}
+
+if (!hasStorage("localStorage")) alert("Warning: Unable to save locally.");
 loadProjects();
+refreshUI();
+
+/*extract to own file*/
+
+const openModal = document.querySelector(".open-modal");
+const closeModal = document.querySelector(".close-modal");
+
+openModal.addEventListener("click", () => {
+  const popupModal = document.querySelector(".popup-modal");
+  const backdrop = document.querySelector(".backdrop");
+  popupModal.classList.add("show");
+  backdrop.classList.add("show");
+});
+
+closeModal.addEventListener("click", () => {
+  const popupModal = document.querySelector(".popup-modal");
+  const backdrop = document.querySelector(".backdrop");
+  popupModal.classList.remove("show");
+  backdrop.classList.remove("show");
+});
