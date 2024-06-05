df6254db0c0b6f818272204c5ec7c37965135396
sam <abluecar@gmail.com>
2024-06-04 20:42:32 -0400

    feat: Close button for hamburger

--- src/nav.js
+++ src/nav.js
@@ -1,12 +1,15 @@
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle("show");
-});
-
 import Checkmark from "./assets/checkmark.svg";
 const logo = document.querySelector(".logo");
 const checkmark = new Image();
 checkmark.src = Checkmark;
 checkmark.classList.add("logo-image");
 logo.insertBefore(checkmark, logo.childNodes[1]);
+
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+const toggle = function () {
+  nav.classList.toggle("show");
+};
+navToggle.addEventListener("click", toggle);
+const navCloseButton = document.querySelector(".nav__close-button");
+navCloseButton.addEventListener("click", toggle);
